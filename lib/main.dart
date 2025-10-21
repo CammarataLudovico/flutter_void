021a0a224fcd72d8f83b1f81693f9ec8adbbbe36
Ludovico Cammarata <cammarataludvico9@gmail.com>
2025-10-21 15:11:11 +0200

    Implemented all the function of the README.md, need to fix a field problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: GreeterWidget(),
-    ),
-  );
+  runApp(const MaterialApp(home: GreeterWidget()));
 }
 
 class GreeterWidget extends StatefulWidget {
@@ -16,49 +12,92 @@ class GreeterWidget extends StatefulWidget {
 }
 
 class _GreeterWidgetState extends State<GreeterWidget> {
-  late final TextEditingController _controller;
+  late final TextEditingController _helloController;
+  late final TextEditingController _yoController;
   String _message = '';
 
   void initState() {
     super.initState();
-    _controller = TextEditingController();
+    _helloController = TextEditingController();
+    _yoController = TextEditingController();
   }
 
-  void dispose() {  }
+  void dispose() {
+    setState(() {
+      _message = "";
+      _helloController.clear();
+      _yoController.clear();
+    });
+  }
 
   void _updateGreeting() {
-    // TODO ...
+    setState(() {
+      if (_helloController.text.isEmpty && _yoController.text.isNotEmpty) {
+        _message = "Yo ${_yoController.text}";
+      } else if (_helloController.text.isNotEmpty && _yoController.text.isEmpty) {
+        _message = "Hello ${_helloController.text}";
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("1.2 personal greeting"),
-      ),
-    body: Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          TextField(
-            controller: _controller,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Enter Your Name',
+      appBar: AppBar(title: const Text("1.2 personal greeting")),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _helloController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter Your Name - HELLO',
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: _updateGreeting,
-            child: const Text('Say Hello'),
-          ),
-          const SizedBox(height: 30),
-          Text(
-            _message,
-          ),
-        ],
+            const SizedBox(height: 20),
+            TextField(
+              controller: _yoController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter Your Name - YO',
+              ),
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: _updateGreeting,
+                    style: ElevatedButton.styleFrom(
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )    
+                    ),
+                    child: const Text('Say Hello'),
+                  ),
+                  const SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: dispose,
+                    style: ElevatedButton.styleFrom(
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )    
+                    ),
+                    child: const Text('Clear'),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            Text(_message),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
