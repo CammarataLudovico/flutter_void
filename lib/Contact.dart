a3e80245ebdf09265f2f7be48f5fac837eb6132f
giovannizol <giovanni.zol.24@stud.itsaltoadriatico.it>
2025-11-18 18:15:55 +0100

    Add Contact class with required fields for first name, last name, and phone numbers

---
+++ lib/Contact.dart
@@ -0,0 +1,10 @@
+class Contact {
+  Contact({
+    required this.firstName,
+    required this.lastName,
+    required this.phoneNumbersList,
+  });
+  String firstName;
+  String lastName;
+  List <String> phoneNumbersList;
+}
